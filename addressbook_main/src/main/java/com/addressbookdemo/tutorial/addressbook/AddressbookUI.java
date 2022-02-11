e9c7e310795a89fd013742a5c41c1fd7790bdef9
Feroz196 <98863206+Feroz196@users.noreply.github.com>
2022-02-11 23:01:12 +0530

    Update AddressbookUI.java

--- addressbook_main/src/main/java/com/addressbookdemo/tutorial/addressbook/AddressbookUI.java
+++ addressbook_main/src/main/java/com/addressbookdemo/tutorial/addressbook/AddressbookUI.java
@@ -145,4 +145,4 @@ public class AddressbookUI extends UI {
     @VaadinServletConfiguration(ui = AddressbookUI.class, productionMode = false)
     public static class MyUIServlet extends VaadinServlet {
     }
-/* I removed } */
+}
